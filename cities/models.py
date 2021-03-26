425282c2973af1ef25a0defdc1b6df114860442f
volter111 <krut.valeriy@gmail.com>
2021-03-25 17:09:33 -0700

    rus admin panel

--- cities/models.py
+++ cities/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class City (models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Город')
 
     def __str__(self):
         return self.name
